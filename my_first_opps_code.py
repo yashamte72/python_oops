51639e9d2629cd1a984c7216540240458279c72d
yashamte72 <yashamte123@gmail.com>
2025-02-19 01:05:30 +0530

    Create my_first_opps_code.py

---
+++ my_first_opps_code.py
@@ -0,0 +1,28 @@
+
+class Student:
+    college_name= "pratibha college"
+
+    def __init__(self,fullname,maths,chemistry,physics):
+        self.name= fullname
+        self.maths= maths
+        self.chemistry=chemistry
+        self.physics=physics
+
+        print("marks of student in the class")
+
+    def average(self):
+        print("Student Marks," , self.name ,self.maths, self.chemistry,self.physics)
+        print("average marks of Student, ", self.name, (self.maths+self.chemistry+self.physics)/3)
+
+    def Avg_Sub(self):
+
+s1=Student("Yash",99,92,93)
+
+
+s2=Student("Vikas",69,45,76)
+
+
+s3=Student("Akash",95,83,88)
+
+
+s3.average()
